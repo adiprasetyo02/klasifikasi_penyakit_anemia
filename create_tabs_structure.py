1435ac514c35bbe03622da8537bbf06a7943dd6d
adiprasetyo02 <100358658+adiprasetyo02@users.noreply.github.com>
2025-04-29 00:15:51 +0700

    Delete create_tabs_structure.py

--- create_tabs_structure.py
+++
@@ -1,5 +0,0 @@
-git init
-create_tabs_structure.py
-git commit -m "Menambahkan folder Tabs dan file home.py serta prediction.py"
-git remote add origin https://github.com/username/repository.git
-git push -u origin main
